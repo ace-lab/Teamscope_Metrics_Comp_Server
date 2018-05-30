e03972b8ac175ad83ca5632ed250dc7b3c28c733
Adnan Hemani <adnan.h@berkeley.edu>
2018-05-29 21:50:30 -0700

    added sad path support

--- metric_tddchecker.py
+++ metric_tddchecker.py
@@ -27,23 +27,23 @@ def compute_tdd_metric(repo):
         #print(commit_timestamp)
         retVal[rollback] = [int(commit_timestamp)]
         for i in parent_commits_list:
-            comp_branch = i.replace("\n", "")
-            #diff_results = subprocess.check_output(["diff-cover", "../commits/{0}.xml".format(rollback), "--compare-branch={0}".format(comp_branch)]).decode("utf-8")
-            print(rollback)
-            print(os.getcwd())
-            diff_results = subprocess.check_output(["diff-cover", "../commits/{0}.xml".format(rollback), "--compare-branch={0}".format(comp_branch)]).decode("utf-8")
-            total_match = re.search(r'Total:(?:\s*)(\d+)', diff_results)
-            if total_match:
-                total = int(str(total_match.group(1)))
-            else:
-                total = 0
-            missing_match = re.search(r'Missing:(?:\s*)(\d+)', diff_results)
-            if missing_match:
-                missing = int(str(missing_match.group(1)))
-            else:
-                missing = 0
             inside_dict = {}
-            inside_dict.update({"commit_before": comp_branch, "total": total, "missing": missing})
+            comp_branch = i.replace("\n", "")
+            if not os.path.exists("../commits/{0}.xml".format(i)):
+                inside_dict.update({"commit_before": comp_branch, "total": -1, "missing": -1, "error": "commit file does not exist"})
+            else:
+                diff_results = subprocess.check_output(["diff-cover", "../commits/{0}.xml".format(rollback), "--compare-branch={0}".format(comp_branch)]).decode("utf-8")
+                total_match = re.search(r'Total:(?:\s*)(\d+)', diff_results)
+                if total_match:
+                    total = int(str(total_match.group(1)))
+                else:
+                    total = 0
+                missing_match = re.search(r'Missing:(?:\s*)(\d+)', diff_results)
+                if missing_match:
+                    missing = int(str(missing_match.group(1)))
+                else:
+                    missing = 0
+                inside_dict.update({"commit_before": comp_branch, "total": total, "missing": missing})
             retVal[rollback].append(inside_dict)
 
     os.chdir("..")
